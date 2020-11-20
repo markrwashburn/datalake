fff9afd3508c4696601c45a4adfd90ae210796f1
markrwashburn <markwashburn@Marks-MBP.hsd1.il.comcast.net>
2020-11-20 12:59:32 -0600

    updated scripts

--- bin/cf-deploy.sh
+++ bin/cf-deploy.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 set +x
+PROFILE='default'
+REGION='us-east-1'
 STACK_NAME='DataLake'
 TEMPLATE_FILE='stack.yaml'
 
+read -p "AWS Region [default=$REGION]: " -e input
+REGION=${input:-$REGION}
+read -p "AWS Profile [default=$PROFILE]: " -e input
+PROFILE=${input:-$PROFILE}
 read -p "Stack Name [default=$STACK_NAME]: " -e input
 STACK_NAME=${input:-$STACK_NAME}
 read -p "Template File [default=$TEMPLATE_FILE]: " -e input
 TEMPLATE_FILE=${input:-$TEMPLATE_FILE}
 
 if [ -f $TEMPLATE_FILE ]; then
-  aws cloudformation deploy --template-file $TEMPLATE_FILE --stack-name $STACK_NAME --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND
+  aws cloudformation deploy --template-file $TEMPLATE_FILE --region $REGION --profile $PROFILE --stack-name $STACK_NAME --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND
 fi
--- bin/cf-package.sh
+++ bin/cf-package.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 set +x
-S3_BUCKET_NAME='dataops-12bd30f0'
+PROFILE='default'
+REGION='us-east-1'
+CF_REPO_BUCKET='dataops-12bd30f0'
 TEMPLATE_FILE='stack.template'
 OUTPUT_TEMPLATE_FILE='stack.yaml'
 
-read -p "S3 Bucket Name [default=$S3_BUCKET_NAME]: " -e input
-S3_BUCKET_NAME=${input:-$S3_BUCKET_NAME}
+read -p "AWS Region [default=$REGION]: " -e input
+REGION=${input:-$REGION}
+read -p "AWS Profile [default=$PROFILE]: " -e input
+PROFILE=${input:-$PROFILE}
+read -p "CF Repository Bucket [default=$CF_REPO_BUCKET]: " -e input
+CF_REPO_BUCKET=${input:-$CF_REPO_BUCKET}
 read -p "Template File [default=$TEMPLATE_FILE]: " -e input
 TEMPLATE_FILE=${input:-$TEMPLATE_FILE}
 read -p "Output Template File [default=$OUTPUT_TEMPLATE_FILE]: " -e input
 OUTPUT_TEMPLATE_FILE=${input:-$OUTPUT_TEMPLATE_FILE}
 
 if [ -f template/$TEMPLATE_FILE ]; then
-  aws cloudformation package --template-file template/$TEMPLATE_FILE --s3-bucket $S3_BUCKET_NAME --output-template-file $OUTPUT_TEMPLATE_FILE
+  aws cloudformation package --region $REGION --profile $PROFILE --s3-bucket $CF_REPO_BUCKET --template-file template/$TEMPLATE_FILE --output-template-file $OUTPUT_TEMPLATE_FILE
 fi
